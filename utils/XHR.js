cff7369b4e50ecf7c23996e618398a2cd762b6e0
ClementR <clementramel@gmail.com>
2022-01-25 23:12:21 +0100

    Update dossier API

--- utils/XHR.js
+++ utils/XHR.js
@@ -1,5 +1,5 @@
 
-const DOMAIN = "http://192.168.1.32:8080/Stage/Stage_API/";
+const DOMAIN = "http://192.168.1.32:8080/Stage/philsUp/Stage_API/";
 
 export default function XHR(pathRequest, callback) {
 
