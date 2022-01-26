468ef8388641e76108b88afc6c6378f6f8e60434
Dylan <dylan77120@live.fr>
2022-01-26 13:56:53 +0100

    drawer en place

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import GroupList from './views/GroupList.js'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Drawer from './components/common/Navigation/Drawer';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <GroupList />
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <NavigationContainer>
+                <Drawer />
+                <StatusBar style="auto"/>
+            </NavigationContainer>
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
